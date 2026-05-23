7c6c68618662dc4b4c454d2c24a217c90a44235d
Heidy Cardenas <140125367+HeidyCardenas1903@users.noreply.github.com>
2026-05-22 23:45:25 -0500

    Estructura inicial de JS

---
+++ script.js
@@ -0,0 +1,6 @@
+const taskForm = document.getElementById("taskForm");
+const taskList = document.getElementById("taskList");
+const message = document.getElementById("message");
+
+let tasks = [];
+let editIndex = null;
