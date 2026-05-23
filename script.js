e42832b70f977586e1464006eebc3b3b852e8e2d
Carlos <rocarlos1205@gmail.com>
2026-05-23 14:47:29 -0500

    Añade funcionalidad para editar tareas

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const message = document.getElementById("message");
 
 let tasks = [];
 let editIndex = null;
+
+function editTask(index) {
+    document.getElementById("title").value = tasks[index].title;
+    document.getElementById("description").value = tasks[index].description;
+    document.getElementById("date").value = tasks[index].date;
+
+    editIndex = index;
+    message.textContent = "Editando tarea seleccionada.";
+    message.style.color = "#f59e0b";
+}
