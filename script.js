fcccee8a3ee1acfabe02b7ef80f3fe47b62dfb46
Cristian <devjunsunsh@gmail.com>
2026-05-23 14:33:56 -0500

    Agrega funcion eliminar tareas

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const message = document.getElementById("message");
 
 let tasks = [];
 let editIndex = null;
+
+function deleteTask(index) {
+  const confirmDelete = confirm("¿Estás seguro de eliminar esta tarea?");
+
+  if (confirmDelete) {
+    tasks.splice(index, 1);
+    message.textContent = "Tarea eliminada correctamente.";
+    message.style.color = "green";
+    renderTasks();
+  }
+}
