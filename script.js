77c80af433f311cb46f53595e6f5160d5a9dcf0c
Jairo <jbuitrago82@soy.sena.edu.co>
2026-05-23 14:41:44 -0500

    Se añada la funcionalidad de visualizacion de tareas

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const message = document.getElementById("message");
 
 let tasks = [];
 let editIndex = null;
+
+function renderTasks() {
+  taskList.innerHTML = "";
+
+  if (tasks.length === 0) {
+    taskList.innerHTML = "<p>No hay tareas registradas.</p>";
+    return;
+  }
+
+  tasks.forEach((task, index) => {
+    const card = document.createElement("div");
+    card.className = "task-card";
+
+    card.innerHTML = `
+      <h3>${task.title}</h3>
+      <p>${task.description}</p>
+      <p><strong>Fecha:</strong> ${task.date}</p>
+    `;
+
+    taskList.appendChild(card);
+  });
+}
